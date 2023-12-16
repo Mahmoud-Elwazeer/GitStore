3c1a69fd72617b12ee40d3967284daf849ead720
Karim-Moharm <karimmoharm222@gmail.com>
2023-12-16 22:58:36 +0200

    update the cart

--- storeOverflow/carts/routes.py
+++ storeOverflow/carts/routes.py
@@ -16,7 +16,7 @@ def addtocart():
     try:
         product_id = request.form.get("product_id")
         color = request.form.get("colors")
-        quantity = request.form.get("quantity")
+        quantity = int(request.form.get("quantity"))
         size = request.form.get("size")
         product  = Product.query.filter_by(id=product_id).first_or_404()
 
@@ -24,7 +24,9 @@ def addtocart():
             productsDict = {product_id: {
                 'name': product.name,
                 'price': float(product.price),
-                'size': size, 'quantity': int(quantity), 'color': color,
+                'size': size,
+                'quantity': quantity,
+                'color': color,
                 'discount': product.discount,
                 'image': product.image_1,
                 'colors': product.color,
@@ -32,11 +34,14 @@ def addtocart():
             }}
             if 'ShooppingCart' in session:
                 if product_id in session['ShooppingCart']:
-                    print('here')
+                    print('here11')
                     for key, value in session['ShooppingCart'].items():
-                        if key == product_id:
+                        print('here222')
+                        if int(key) == int(product_id):
+                            print('here222')
+                            print(type(quantity), type(key), type(product_id))
                             session.modified = True
-                            value['quantity'] =  int(value['quantity']) + 1
+                            value['quantity'] += 1
                 else:
                     session['ShooppingCart'] = mergeDict(session['ShooppingCart'], productsDict)
                     return redirect(request.referrer)
@@ -53,11 +58,11 @@ def addtocart():
 def getcart():
     if 'ShooppingCart' not in session:
         return redirect(url_for('home'))
-    total = 0
+    total = float(0)
     for key, value in session['ShooppingCart'].items():
-        new_price = value['price'] - (value['discount'] / 100.0 )* value['price']
-        sub_total = new_price * int(value['quantity'])
-        total += sub_total
+        new_price = float( value['price']) - float((value['discount'] / 100.0 )) * float(value['price'])
+        sub_total = float(new_price) * int(value['quantity'])
+        total += float(sub_total)
 
     return render_template('products/cart.html', total=total)
 
@@ -70,3 +75,37 @@ def clearcart():
         return redirect(url_for('home'))
     except Exception as e:
         flash (e, 'danger')
+
+
+@app.route('/updatecart/<int:id>', methods=["POST"])
+def updatecart(id):
+    if 'ShooppingCart' not in session:
+        return redirect(url_for('home'))
+
+    color = request.form.get("colors")
+    quantity = int(request.form.get("quantity"))
+    size = request.form.get("size")
+    try:
+        session.modified = True
+        session['ShooppingCart'].get(str(id))['quantity'] = quantity
+        session['ShooppingCart'].get(str(id))['color'] = color
+        session['ShooppingCart'].get(str(id))['size'] = size
+        # print(type(session['ShooppingCart'].get(str(id))['color']))
+        # print(type(session['ShooppingCart'].get(str(id))['quantity']))
+        
+    except Exception as e:
+        flash(e, 'danger')
+    return redirect(url_for('getcart'))
+
+
+@app.route('/removecart/<int:id>', methods=["POST"])
+def removecart(id):
+    if 'ShooppingCart' not in session:
+        return redirect(url_for('home'))
+    try:
+        session.modified = True
+        session['ShooppingCart'].pop(str(id), None)
+    
+    except Exception as e:
+        flash(e, 'danger')
+    return redirect(url_for('getcart'))
